714de8081bb07e8354d8e7cfa6a1ae0950f42ff1
Isak Pettersson <isak.movitz.pettersson@gmail.com>
2022-02-14 15:13:13 +0100

    cleaned up and finished Tutorial 1

--- Dataset/Tutorial_1/imports.py
+++
@@ -1,5 +0,0 @@
-from datasets import load_dataset
-from transformers import pipeline
-import rubrix as rb
-from datasets import Dataset, Features, Value, ClassLabel
-from transformers import AutoTokenizer
