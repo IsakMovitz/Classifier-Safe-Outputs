04a0602a450ace306f8366e23050f43711c4f87e
Isak Pettersson <isak.movitz.pettersson@gmail.com>
2022-02-25 14:50:35 +0100

    tried pre filtering Oscar data on racist keywords

--- AnnotationPipeline/clean_annotated.py
+++ AnnotationPipeline/clean_annotated.py
@@ -2,6 +2,9 @@ import json
 
 def clean_annotation(input_filename, output_filename):
     cleaned_data = []
+
+    id_list = []
+
     #Open annotation file from Prodigy:
     with open(input_filename, 'r') as json_file:
         json_list = list(json_file)
@@ -9,17 +12,20 @@ def clean_annotation(input_filename, output_filename):
     for json_str in json_list:
         line_dict = json.loads(json_str)
         id_nr = line_dict['id']
-        text_sample = line_dict['text']
-        starting_index = line_dict['starting_index']
-        span_length = line_dict['span_length']
-        toxic_class = line_dict['toxic_class']
+        if id_nr not in id_list:
+            id_list.append(id_nr)
+            
+            text_sample = line_dict['text']
+            starting_index = line_dict['starting_index']
+            span_length = line_dict['span_length']
+        
 
-        if line_dict['answer'] == 'accept': # 1
-            cleaned_data.append({"id":id_nr,"text":text_sample,"starting_index":starting_index,
-            "span_length":span_length,"sexist_class":toxic_class,"racist_class":1})
-        else:
-            cleaned_data.append({"id":id_nr,"text":text_sample,"starting_index":starting_index,
-            "span_length":span_length,"sexist_class":toxic_class,"racist_class":0})
+            if line_dict['answer'] == 'accept': # 1
+                cleaned_data.append({"id":id_nr,"text":text_sample,"starting_index":starting_index,
+                "span_length":span_length,"sexually_explicit":1})
+            else:
+                cleaned_data.append({"id":id_nr,"text":text_sample,"starting_index":starting_index,
+                "span_length":span_length,"sexually_explicit":0})
 
     #Save cleaned back to jsonl format:
     with open(output_filename, 'w', encoding='utf-8') as f:
@@ -30,4 +36,8 @@ def clean_annotation(input_filename, output_filename):
                     f.write(json.dumps(item,ensure_ascii=False))
 
 
-clean_annotation("Data/annotated_racist_dataset.jsonl","Data/clean_annotated_racist.jsonl")
+clean_annotation("Data/annotated_sexist_dataset.jsonl","Data/clean_annotated_sexist.jsonl")
+
+
+
+
--- AnnotationPipeline/load_to_jsonl.py
+++ AnnotationPipeline/load_to_jsonl.py
@@ -13,8 +13,8 @@ DatasetDict({
 '''
 
 ### Loading the data ###
-oscar_dataset = load_dataset('oscar-corpus/OSCAR-2109', 'deduplicated_sv', use_auth_token=True)
-training_data = oscar_dataset["train"] 
+# oscar_dataset = load_dataset('oscar-corpus/OSCAR-2109', 'deduplicated_sv', use_auth_token=True)
+# training_data = oscar_dataset["train"] 
 
 '''
 id: id of the instance
@@ -30,7 +30,7 @@ Right now just ignore it
 
 '''
 
-### Parsing into JSONL format ###
+### Parsing into JSONL format from Huggingface ###
 
 def parse_to_jsonl(data,span_length,filename,nr_samples):
     random_samples = []    
@@ -61,13 +61,37 @@ def parse_to_jsonl(data,span_length,filename,nr_samples):
             else:
                 f.write(json.dumps(item,ensure_ascii=False))
 
+#parse_to_jsonl(training_data,15,"Data/raw_racism_data_10.jsonl",10)  # Since index 41 is < 15 in length
 
-parse_to_jsonl(training_data,15,"Data/raw_oscar_50000.jsonl",50000)  # Since index 41 is < 15 in length
+
+### Sampling spans from a jsonl file with id,text ###
+
+def sample_span_from_jsonl(input_filename,output_filename,span_length):
+
+    with open(input_filename, 'r') as json_file:
+            json_list = list(json_file)
+
+    random_samples = []
+    for json_str in json_list:
+        line_dict = json.loads(json_str)
+
+        id_nr = line_dict['id']
+        instance_text = line_dict['text']
+        instance_text_list = instance_text.split(' ')
+
+        length = len(instance_text_list)
+        if length > span_length:
+            starting_index = random.randrange(0,length - span_length)
+            random_sample_list = instance_text_list[starting_index:starting_index + span_length]
+
+            # Back to string
+            str1 = " " 
+            text_sample = str1.join(random_sample_list)
+            random_samples.append({"id":id_nr,"text":text_sample,"starting_index":starting_index,"span_length":span_length})
+        
+    with open(output_filename, 'w', encoding='utf-8') as f:
+            for item in random_samples:
+                f.write(json.dumps(item,ensure_ascii=False) + "\n")
 
 
-def print_from_jsonl(filename):
-    with open("data/" + filename) as f:
-        for line in f:
-            print(line)
-
-#print_from_jsonl("raw_oscar_100.jsonl")
+#sample_span_from_jsonl('Data/pre_search_racism_whole.jsonl','Data/samples_racism.jsonl',15)
