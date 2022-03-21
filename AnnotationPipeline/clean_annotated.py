c93fd6d3fbb9fceffac1bfd6ef5de51458802864
Isak Pettersson <isak.movitz.pettersson@gmail.com>
2022-03-21 17:14:10 +0100

    finished

--- AnnotationPipeline/clean_annotated.py
+++ AnnotationPipeline/clean_annotated.py
@@ -18,17 +18,20 @@ def clean_annotation(input_filename, output_filename):
             text_sample = line_dict['text']
             thread = line_dict['thread']
             thread_id = line_dict['thread_id']
-            keyword =  line_dict['keyword']
+            keyword =  line_dict['keyword'].strip(" ")
             starting_index = line_dict['starting_index']
             span_length = line_dict['span_length']
-        
 
+            # For extra with franscesca
+
+            toxic = line_dict['TOXIC']
+        
             if line_dict['answer'] == 'accept': # 1
-                cleaned_data.append({"id":id_nr,"thread":thread,"thread_id":thread_id,"text":text_sample,"starting_index":starting_index,
-                "span_length":span_length,"TOXIC":1})
+                cleaned_data.append({"id":id_nr,"thread":thread,"thread_id":thread_id, "text":text_sample, "keyword": keyword ,"starting_index":starting_index,
+                "span_length":span_length,"TOXIC":toxic, "TOXIC_2": 1})
             else:
-                cleaned_data.append({"id":id_nr,"thread":thread,"thread_id":thread_id,"text":text_sample,"starting_index":starting_index,
-                "span_length":span_length,"TOXIC":0})
+                cleaned_data.append({"id":id_nr,"thread":thread,"thread_id":thread_id,"text":text_sample,"keyword": keyword,"starting_index":starting_index,
+                "span_length":span_length,"TOXIC":toxic, "TOXIC_2": 0})
 
     #Save cleaned back to jsonl format:
     with open(output_filename, 'w', encoding='utf-8') as f:
@@ -39,8 +42,10 @@ def clean_annotation(input_filename, output_filename):
                     f.write(json.dumps(item,ensure_ascii=False))
 
 
-clean_annotation("NEW_DATA/Annotated_data/20span_teach_1000_flashback_keyword_dataset.jsonl","NEW_DATA/Clean_annotated_data/clean_20span_teach_1000_flashback_keyword_dataset.jsonl")
+#clean_annotation("./NEW_DATA/Annotated_data/20span_teach_1000_flashback_keyword_dataset.jsonl","./NEW_DATA/Clean_annotated_data/clean_20span_teach_1000_flashback_keyword_dataset.jsonl")
 
+# Franscesca
 
+clean_annotation("./NEW_DATA/Annotated_data/franscesca_test_dataset.jsonl","./NEW_DATA/Clean_annotated_data/clean_franscesca_test_dataset.jsonl" )
 
 
