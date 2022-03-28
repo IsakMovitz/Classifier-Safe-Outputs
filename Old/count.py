3642d705d3e758495018959c1520d083320a6be2
Isak Pettersson <isak.movitz.pettersson@gmail.com>
2022-03-28 20:18:06 +0200

    added annotated file

--- Old/count.py
+++ Old/count.py
@@ -1,6 +1,12 @@
 import json  
+# 291 / 1000 , for keyword 20 span
 
-input_filename = 'Data/test_data.jsonl'
+# 
+
+
+
+
+input_filename = '../AnnotationPipeline/NEW_DATA/Clean_annotated_data/clean_20span_teach_1000_flashback_keyword_dataset.jsonl'
 
 with open(input_filename, 'r') as json_file:
         json_list = list(json_file)
@@ -9,7 +15,8 @@ it = 0
 for json_str in json_list:
     line_dict = json.loads(json_str)
 
-    it += 1
+    if line_dict['TOXIC'] == 1:
+        it += 1
 
 print(it)
 
